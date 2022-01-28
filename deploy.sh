6785ebe17f7a33443edf7d84c17b4ced6a6d8682
Abhishek Pednekar <abhi_ap@hotmail.com>
2022-01-28 22:15:26 +0530

    TravisCI integration

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xe
+
+server=157.245.101.118
+
+if [ $TRAVIS_BRANCH == 'main' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  cd client
+  rsync -a .next package.json package-lock.json public travis@$server:/home/listings/site
+  echo "Deployed successfully!"
+else
+  echo "Not deploying, since the branch isn't main."
+fi
+
+ssh travis@$server 'pm2 restart all' --update-env
