2e708e29a5cadbb8778eca4334581577d29b9b04
Abhishek Pednekar <abhi_ap@hotmail.com>
2022-02-01 21:38:50 +0530

    Updated the FPA contact section

--- components/listings/FullPageAd.jsx
+++ components/listings/FullPageAd.jsx
@@ -244,7 +244,7 @@ const FullPageAd = ({ adData, apartmentName, username, disablePrint }) => {
               Contact details
             </h3>
             <div className="flex justify-center">
-              <div className="pt-5 pb-10 print:py-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-2 print:gap-2 gap-5 place-items-start lg:place-items-center px-5 lg:px-10">
+              <div className="pt-5 pb-10 print:py-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-2 print:gap-2 gap-5 lg:gap-x-14 place-items-start lg:place-items-center px-5">
                 <div className="flex items-center">
                   <span className="text-sm text-zinc-500">Owner:</span>
                   &nbsp;
